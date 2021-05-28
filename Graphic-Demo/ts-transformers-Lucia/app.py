40fd80d022bde35cde8bdd574b717bd990c31df8
luruz <100363940@alumnos.uc3m.es>
2021-05-28 17:12:59 +0200

    estilo retocado

--- Graphic-Demo/ts-transformers-Lucia/app.py
+++ Graphic-Demo/ts-transformers-Lucia/app.py
@@ -8,7 +8,7 @@
 
 
 import re
-import requests                                    # https://www.youtube.com/c/PythonProgramming4u
+import requests                                    
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
 
@@ -31,20 +31,8 @@ def getvalue():
         rem = "Error, empty form"
         return render_template('index.html', rem=rem)
     else:
-        # response = requests.get(str(url_str))
-        # cap = re.findall('[0-9]+\.ece', url_str)
-        # cap1 = re.findall('[0-9]+', cap[0])
-        # divid = "content-body-14269002-" + cap1[0]
-        # soup = BeautifulSoup(response.content, "html.parser")
-        # layout = soup.find("div", attrs={"id": divid})
-        # paragraphs = layout.find_all('p')
-        # SentRank = ms.summa(paragraphs)
-        # # def getvalues1(paragraphs):
-        # sentGen = ms.gensima(paragraphs)
-        # return render_template('index.html', sentGen=sentGen, SentRank=SentRank)
-
         summary = ls.text_summarization(selection,text_input)
-        title = "Summarry generated with "+selection+" model."
+        title = "Summary generated with "+selection+" model."
         return render_template('index.html', setText=summary, setTitle=title)
 
 
